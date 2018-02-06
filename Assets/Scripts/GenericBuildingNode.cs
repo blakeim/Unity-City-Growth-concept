4617b60f2bff7ce48e2e1c6e9d1cf91dc7cb3a86
Ian Blake <IanMBlake13@gmail.com>
2018-02-06 00:03:43 -0500

    Began building type scan logic
    Begining of the logic to scan the area and weight the type of building that is generated when the building is built.

--- Assets/Scripts/GenericBuildingNode.cs
+++ Assets/Scripts/GenericBuildingNode.cs
@@ -16,6 +16,7 @@ public class GenericBuildingNode : BuildingNode {
 
 	[SerializeField]
 	int buildingTypes;
+	
 	// Use this for initialization
 	void Start () {
 		print("Locatoin \t" + this.transform.position);
@@ -62,5 +63,38 @@ public class GenericBuildingNode : BuildingNode {
 	public override BuildingClass getBuildingClass(){
 
 		return concreteImplementation.getBuildingClass();
+	}
+
+	private int[] buildingWeights(){
+
+		int[] weightArray = new int[buildingTypes];
+
+		/*this loop iterates through the array of weights, that starts empty, and see if any buildings
+		of the corosponding class are found within a set distance. I really hate this. I 
+		hate how not extendable this is, it makes my soul hurt a little bit, but I want to
+		try to proof out this concept, I'll refine it if it works. */
+		foreach(int i in weightArray){
+			
+		}
+
+		return weightArray;
+	}
+
+	private int checkBuildingsAround(BuildingClass classToCheck){
+
+		RaycastHit hit;
+
+        Vector3 p1 = transform.position;
+        float distanceToObstacle = 0;
+
+		/*throw out a sphere, and compare the hits building class to the class given as part of the check call
+		Calculate a weight (that maybe should be a float?) and return it to the caller. Those weights will be 
+		used to fudge the random number in the direction of the building that should be spawned*/	
+        if (Physics.SphereCast(p1, transform.localScale.y, transform.forward, out hit, 10))
+        {
+            distanceToObstacle = hit.distance;
+        }
+
+		return 0;
 	}
 }
